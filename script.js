e00afbeb0ddf0c2a991c6a90ff044266a834c70a
Suj <sujithpost@gmail.com>
2021-10-10 18:20:26 +0100

    first commit.

---
+++ script.js
@@ -0,0 +1,157 @@
+(function() 
+ {
+  var allQuestions = [{
+    question: "TheEEEEEE tree sends downroots from its branches to the soil is know as:",
+    options: ["Oak", "Pine", "Banyan", "Palm"],
+    answer: 1
+  }, {
+    question: "In what langauge does a comment start with //?",
+    options: ["Python", "javascript", "ruby", "HTML"],
+    answer: 1
+  }, {
+    question: "Non Metal that remains liquid at room temprature is",
+    options: ["Phophorous", "Bromine", "Clorine","Helium"],
+    answer: 1
+  },{
+    question: "Which of the following is used in Pencils ?",
+    options: ["Graphite", "Silicon", "Charcoal", "Phosphorous"],
+    answer: 1
+  }, {
+    question: "Chemical formula of water ?",
+    options: ["NaA1O2", "H2O", "Al2O3", "CaSiO3"],
+    answer: 1
+  },{
+    question: "The gas filled in electric bulb is ?",
+    options: ["Nitrogen", "Hydrogen", "Carbon Dioxide", "Oxygen"],
+    answer: 1
+  },{
+    question: "Whashing soda is the comman name for",
+    options: ["Sodium Carbonate", "Calcium Bicarbonate", "Sodium Bicarbonate", "Calcium Carbonate"],
+    answer: 1
+  },{ 
+    question: "Which gas is not known as green house gas ?",
+    options: ["Methane", "Nitrous oxide", "Carbon Dioxide", "Hydrogen"],
+    answer: 1
+  },{
+    question: "The hardest substance availabe on earth is",
+    options: ["Gold", "Iron", "Diamond", "Platinum"],
+    answer: 1
+  },{
+    
+  },{
+    question: "Use
+    question: "Used as a lubricant",
+    options: ["Graphite", "Silica", "Iron Oxide", "Diamond"],
+    answer: 1
+    }];
+  
+  var quesCounter = 0;
+  var selectOptions = [];
+  var quizSpace = $('#quiz');
+    
+  nextQuestion();
+    
+  $('#next').click(function () 
+    {
+        chooseOption();
+        if (isNaN(selectOptions[quesCounter])) 
+        {
+            alert('Please select an option !');
+        } 
+        else 
+        {
+          quesCounter++;
+          nextQuestion();
+        }
+    });
+  
+  $('#prev').click(function () 
+    {
+        chooseOption();
+        quesCounter--;
+        nextQuestion();
+    });
+  
+  function createElement(index) 
+    {
+        var element = $('<div>',{id: 'question'});
+        var header = $('<h2>Question No. ' + (index + 1) + ' :</h2>');
+        element.append(header);
+
+        var question = $('<p>').append(allQuestions[index].question);
+        element.append(question);
+
+        var radio = radioButtons(index);
+        element.append(radio);
+
+        return element;
+    }
+  
+  function radioButtons(index) 
+    {
+        var radioItems = $('<ul>');
+        var item;
+        var input = '';
+        for (var i = 0; i < allQuestions[index].options.length; i++) {
+          item = $('<li>');
+          input = '<input type="radio" name="answer" value=' + i + ' />';
+          input += allQuestions[index].options[i];
+          item.append(input);
+          radioItems.append(item);
+        }
+        return radioItems;
+  }
+  
+  function chooseOption() 
+    {
+        selectOptions[quesCounter] = +$('input[name="answer"]:checked').val();
+    }
+   
+  function nextQuestion() 
+    {
+        quizSpace.fadeOut(function() 
+            {
+              $('#question').remove();
+              if(quesCounter < allQuestions.length)
+                {
+                    var nextQuestion = createElement(quesCounter);
+                    quizSpace.append(nextQuestion).fadeIn();
+                    if (!(isNaN(selectOptions[quesCounter]))) 
+                    {
+                      $('input[value='+selectOptions[quesCounter]+']').prop('checked', true);
+                    }
+                    if(quesCounter === 1)
+                    {
+                      $('#prev').show();
+                    } 
+                    else if(quesCounter === 0)
+                    {
+                      $('#prev').hide();
+                      $('#next').show();
+                    }
+                }
+              else 
+                {
+                    var scoreRslt = displayResult();
+                    quizSpace.append(scoreRslt).fadeIn();
+                    $('#next').hide();
+                    $('#prev').hide();
+                }
+        });
+    }
+  
+  function displayResult() 
+    {
+        var score = $('<p>',{id: 'question'});
+        var correct = 0;
+        for (var i = 0; i < selectOptions.length; i++) 
+        {
+          if (selectOptions[i] === allQuestions[i].answer) 
+          {
+            correct++;
+          }
+        }
+        score.append('You scored ' + correct + ' out of ' +allQuestions.length);
+        return score;
+  }
+})();
